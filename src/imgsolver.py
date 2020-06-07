c3b9831383f291a17fda4ccabecc68a2150198d8
The-H4CKER <66517600+The-H4CKER@users.noreply.github.com>
2020-06-07 23:19:48 +0100

    Update imgsolver.py

--- src/imgsolver.py
+++ src/imgsolver.py
@@ -2,14 +2,15 @@
 
 from PIL import Image
 
-image = Image.open("img.PNG") #opens image
-pixels = []
+image = Image.open("img.PNG") #opens image file
+pixels = [] #list of image data
 
 for i in range(len(image.getcolors())):
     x = i*image.size[0]/len(image.getcolors())
+    #generates different x-position for each colour
     pixels.append(list(image.getpixel((x,0))[:-1]))
+    #adds the RGB value of each colour to pixels list
 
 msg = "".join(["".join([chr(p) for p in pixel]) for pixel in pixels])
-
+#converts nested list to string
 print(msg)
-
