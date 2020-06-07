929561718c6a1741134f604d8c61e591bf683fb1
The-H4CKER <66517600+The-H4CKER@users.noreply.github.com>
2020-06-07 22:52:14 +0100

    Add files via upload

--- src/imgsolver.py
+++ src/imgsolver.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+
+from PIL import Image
+
+image = Image.open("img.PNG") #opens image
+pixels = []
+
+for i in range(len(image.getcolors())):
+    x = i*image.size[0]/len(image.getcolors())
+    pixels.append(list(image.getpixel((x,0))[:-1]))
+
+msg = "".join(["".join([chr(p) for p in pixel]) for pixel in pixels])
+
+print(msg)
 
